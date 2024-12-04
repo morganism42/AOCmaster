40b0db8ee251b51ae6d24b529034db33a53aaed9
Liam Stregger <liamstregger@gmail.com>
2024-12-03 23:11:29 -0800

    preumptive file creation 2015/2024

---
+++ 2015/dec23.py
@@ -0,0 +1,10 @@
+from aocd import get_data, submit
+def solve(data):
+    # Your solution code here
+    pass
+
+if __name__ == "__main__":
+    data = get_data(day=23, year=2024)
+    result = solve(data)
+    print(result)
+    # submit(result, part="a", day=X, year=2015)
