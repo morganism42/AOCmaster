0a6e0544c0a8ae00096b8833781b43bd86667c50
Liam Stregger <liamstregger@gmail.com>
2024-12-03 23:27:56 -0800

    preumptive file creation 2015/2024

--- 2024/dec7.py
+++ 2024/dec7.py
@@ -1,11 +1,44 @@
 from aocd import get_data, submit
-
-def solve(data):
+def solve1(test=True):
     # Your solution code here
-    pass
+    testdata = ''''''
+    testans = ''
+    if test:
+		data = testdata
+	else:
+		data = get_data(day=7, year=2024)
+    
+    
+    
+    
+    
+    
+    if test and result == testans:
+    	print("Test passed")
+    elif test and result != testans:
+		print("Test failed")
+	elif not test:
+		print(result)
+        submit(result, part="a", day=X, year=2024)
+    
 
-if __name__ == "__main__":
-    data = get_data(day=7, year=2024)
-    result = solve(data)
-    print(result)
-    # submit(result, part="a", day=X, year=2024)
+def solve2(test=True):
+    # Your solution code here
+    testdata = ''''''
+    testans = ''
+    if test:
+		data = testdata
+	else:
+		data = get_data(day=7, year=2024)
+    
+    
+    
+    
+    
+    if test and result == testans:
+    	print("Test passed")
+    elif test and result != testans:
+		print("Test failed")
+    elif not test:
+		print(result)
+        submit(result, part="a", day=X, year=2024)
--- filecreator.py
+++ filecreator.py
@@ -5,14 +5,48 @@ def create_files(year, start=1, end=25):
 		filepath = f'C:\\Users\\liam\\Documents\\Python Scripts\\AOCmaster\\{year}\\dec{day}.py'
 		with open(filepath, "w") as f:
 			f.write(f'''from aocd import get_data, submit
-def solve(data):
+def solve1(test=True):
     # Your solution code here
-    pass
+    testdata = \'\'\'\'\'\'
+    testans = \'\'
+    if test:
+		data = testdata
+	else:
+		data = get_data(day={day}, year={year})
+    
+    
+    
+    
+    
+    
+    if test and result == testans:
+    	print("Test passed")
+    elif test and result != testans:
+		print("Test failed")
+	elif not test:
+		print(result)
+        submit(result, part="a", day=X, year={year})
+    
 
-if __name__ == "__main__":
-    data = get_data(day={day}, year=2024)
-    result = solve(data)
-    print(result)
-    # submit(result, part="a", day=X, year={year})''')
+def solve2(test=True):
+    # Your solution code here
+    testdata = \'\'\'\'\'\'
+    testans = \'\'
+    if test:
+		data = testdata
+	else:
+		data = get_data(day={day}, year={year})
+    
+    
+    
+    
+    
+    if test and result == testans:
+    	print("Test passed")
+    elif test and result != testans:
+		print("Test failed")
+    elif not test:
+		print(result)
+        submit(result, part="a", day=X, year={year})''')
 		f.close()
 		print(f"Created {filepath}")
